8b04061d210afb63ef65eaba7ec6b7690406492f
BiggsSteven <Biggs.Steven.M@gmail.com>
2015-04-08 20:56:52 -0400

    Revert "6.2.1.2"
    This reverts commit 399e5de132e0a030f1534e8779953cee341321bd.

--- src/clite/Main.java
+++ src/clite/Main.java
@@ -7,7 +7,7 @@ package clite;
 public class Main {
 
     public static void main(String[] args) {
-        String filename = "programs/functions.cpp";
+        String filename = "programs/sort2.cpp";
     	System.out.println("Begin parsing... " + filename);
     	Parser parser  = new Parser(new Lexer(filename));
         Program prog = parser.program();
@@ -16,7 +16,6 @@ public class Main {
         System.out.println("\nBegin type checking...");
         TypeMap map = StaticTypeCheck.typing(prog.globals);
         map.display();
-        StaticTypeCheck.V(prog, map);
 //
 //        Program out = TypeTransformer.T(prog, map);
 //        System.out.println("\nTransformed Abstract Syntax Tree");
